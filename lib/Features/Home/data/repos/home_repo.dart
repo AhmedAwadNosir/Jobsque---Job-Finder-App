f769ae905f87f83dc423e7d09aa867324bdfec5b
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-09-24 19:06:22 +0300

    create Home rep methodes and failure file

---
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:jobsque_jobfinder/Core/Utils/Errors/failure.dart';
+import 'package:jobsque_jobfinder/Features/Home/data/models/jop_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<JopModel>>> fetchSuggestedJop();
+  Future<Either<Failure,List<JopModel>>> fetchRecentJop();
+}
